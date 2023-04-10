c25ff57cfc9ccd6e2fe69d7b36fcf731578561f6
aaronbush3 <aaronbush3@gmail.com>
2023-04-10 09:45:02 -0400

    Update gitignore, add env variable for secret key

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+import os
 from flask_login import LoginManager
+from dotenv import load_dotenv
+load_dotenv()
+
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -9,7 +13,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
