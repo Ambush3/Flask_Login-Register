229f8d788bdfa32969ca2e1fd87629517a893a11
aaronbush3 <aaronbush3@gmail.com>
2023-04-19 10:09:26 -0400

    Change access to database when running Docker

--- website/__init__.py
+++ website/__init__.py
@@ -14,7 +14,8 @@ DB_NAME = "database.db"
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI', f'sqlite:///{DB_NAME}')
     db.init_app(app)
 
     from .views import views
